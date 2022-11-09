9e154277b816dd5935189758862fe7dee678f2a3
sonadro <sondre.rooth@gmail.com>
2022-11-09 13:01:49 +0100

    Upload & fetch documents

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,21 @@
-console.log(db);
+const db = firebase.firestore();
+
+const addDoc = function() {
+    const obj = {
+        boolField: true,
+        numField: 10,
+        textField: 'texters'
+    }
+    db.collection('testCollection').add(obj).then(() => {
+        console.log('object added', obj);
+    }).catch(err => console.error(err));
+}
+
+const getDocs = function() {
+    db.collection('testCollection').get().then(snapshot => {
+        snapshot.docs.forEach(doc => {
+            const data = doc.data();
+            console.log(data);
+        });
+    }).catch(err => console.error(err));
+}

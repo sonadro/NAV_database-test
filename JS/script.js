c48ecfa6547c188681af90bae78c7aa507812c7c
sonadro <sondre.rooth@gmail.com>
2022-11-23 15:14:57 +0100

    Functional form

--- JS/script.js
+++ JS/script.js
@@ -33,9 +33,9 @@ const getDocs = function() {
 }
 
 const genTemplate = function(obj) {
-    let deceased = 'Død';
+    let deceased = 'Lever';
     if (JSON.parse(obj.deceased)) {
-        deceased = 'Lever'
+        deceased = 'Død'
     }
     const template = `
         <div class="userCardContainer">
@@ -56,7 +56,60 @@ getDocs();
 submitBtn.addEventListener('click', e => {
     e.preventDefault();
     const parents = [grunnleggendeDataForm.forelder1.value, grunnleggendeDataForm.forelder2.value];
-    // const NAV = [];
+    const NAV = {
+        member: NAVForm.medlem.value,
+        disabled: NAVForm.ufor.value,
+        pensioner: NAVForm.pensjonist.value,
+        workInjury: NAVForm.yrkesskade.value,
+        bonusJobPay: NAVForm.dagpenger.value,
+        sickPay: NAVForm.sykepenger.value
+    };
+
+    const economicStatus = {
+        bankAccount: økonomiskeForholdForm.bankkonto.value,
+        creditCard: økonomiskeForholdForm.kredittkort.value,
+        loan: økonomiskeForholdForm.lan.value,
+        accessToData: økonomiskeForholdForm.fullmakt.value
+    };
+
+    const incomeCurrent = {
+        income1: inntekt2022Form.inntekt1.value,
+        income2: inntekt2022Form.inntekt2.value,
+        income3: inntekt2022Form.inntekt3.value,
+        income4: inntekt2022Form.inntekt4.value,
+        income5: inntekt2022Form.inntekt5.value,
+        income6: inntekt2022Form.inntekt6.value,
+        income7: inntekt2022Form.inntekt7.value,
+        income8: inntekt2022Form.inntekt8.value
+    };
+
+    const incomePast = {
+        income2019: inntektFortidForm.inntekt2019.value,
+        income2020: inntektFortidForm.inntekt2020.value,
+        income2021: inntektFortidForm.inntekt2021.value
+    };
+
+    const lifeStatus = {
+        horriblySick: livssituasjonForm.alvorligSyk.value,
+        refugee: livssituasjonForm.flyktning.value,
+        pregnant: livssituasjonForm.gravid.value
+    };
+
+    const lånekassen = {
+        scholarship: lånekassenForm.stipend.value,
+        studyInEU: lånekassenForm.studiested.value
+    };
+
+    const work = {
+        freelancer: arbeidsForholdForm.freelancer.value,
+        jobStatus: arbeidsForholdForm.forholdStatus.value,
+        sector: arbeidsForholdForm.sektor.value,
+        workHoursLast: arbeidsForholdForm.forrigeManed.value,
+        workHoursCurrent: arbeidsForholdForm.denneManed.value
+    };
+
+    const advancedInfo = [economicStatus, lifeStatus, NAV, lånekassen, work, incomePast, incomeCurrent];
+
     const userObject = {
         firstName: grunnleggendeDataForm.fornavn.value,
         lastName: grunnleggendeDataForm.etternavn.value,
@@ -70,7 +123,8 @@ submitBtn.addEventListener('click', e => {
         children: grunnleggendeDataForm.barn.value,
         postalCode: grunnleggendeDataForm.postnummer.value,
         country: grunnleggendeDataForm.land.value,
-        homeYears: grunnleggendeDataForm.botid.value
+        homeYears: grunnleggendeDataForm.botid.value,
+        advancedInfo: advancedInfo
     };
 
     addDoc(userObject, 'personer');

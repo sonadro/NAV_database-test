713fcfd1f5e3a67cc4f28588c21c3242c55d6d92
Tayro <adventuregamr@gmail.com>
2022-12-14 11:45:55 +0100

    Escape lukker nå avansertInfoKort

--- JS/script.js
+++ JS/script.js
@@ -53,7 +53,7 @@ const genTemplate = function(obj, id) {
     if (obj.status == 0) {
         status = 'Lever'
     }
-    console.log(id);
+    //console.log(id);
 
     const template = `
         <div class="userCardContainer" id="${id}">
@@ -95,6 +95,7 @@ advancedInfoCard.addEventListener('click', e => {
 
 })
 
+let advInfoCheck;
 
 // function for opening the advanced info cards
 let openAdvancedInfo = id => {
@@ -245,16 +246,27 @@ let openAdvancedInfo = id => {
             }
         });
     });
+    advInfoCheck = true;
+    //console.log(advInfoCheck);
 }
 let closeAdvancedInfo = () => {
     advancedInfoCard.style.display = 'none';
     body.classList.toggle("noScroll");
     advancedInfoCardContainer.style.zIndex = "-1";
     advancedInfoCardContainer.style.backgroundColor = "rgb(0, 0, 0, 0)";
+    advInfoCheck = false;
 }
 advancedInfoCardContainer.addEventListener('click', e => {
     //console.log(e.target);
     if (e.target == advancedInfoCardContainer){
+        closeAdvancedInfo();
+    }
+});
+
+window.addEventListener('keydown', e => {
+    //console.log(e.key);
+    if(e.key == 'Escape' && advInfoCheck == true){
+        //console.log(e.key);
         closeAdvancedInfo();
     }
 })

b45ea0e90fe6f3f56634688fe162815c65f6760b
Tayro <adventuregamr@gmail.com>
2023-02-01 10:41:27 +0100

    Form ligger nå på en egen side

--- server.js
+++ server.js
@@ -15,12 +15,15 @@ server.listen(port);
 console.log(`Listening for request on port ${port}`);
 
 // redirects
-server.get('/', (req, res) => res.redirect('/form'));
+server.get('/', (req, res) => res.redirect('/hjem'));
 
 // routes
-server.get('/form', (req, res) => res.render('index'));
+server.get('/hjem', (req, res) => res.render('index'));
 server.get('/om-siden', (req, res) => res.render('om'));
+
+// admin routes
 server.get('/admin', (req, res) => res.render('adminPages/admin'));
+server.get('/form', (req, res) => res.render('adminPages/form'));
 
 // 404
 server.use((req, res) => res.status(404).render('404.ejs'));

49176b2ebd47f4e886bbd51daad6393e37d76041
ShieldyArmor <Thegreen_dragon@hotmail.com>
2023-02-08 14:57:17 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,8 +6,8 @@ const router = Router();
 
 router.get('/', authController.home_get);
 router.get('/om-siden', authController.om_get);
-router.get('/admin', requireAuth, authController.adminMain_get);
-router.get('/admin/form', requireAuth, authController.adminForm_get);
+router.get('/admin', authController.adminMain_get);
+router.get('/admin/form', authController.adminForm_get);
 router.get('/login', authController.login_get);
 router.get('/create', authController.create_get);
 router.get('/logout', authController.logout_get);

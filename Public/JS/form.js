f69ede81add3edc6d9a74e337abb6ffb6dc43a2d
ShieldyArmor <Thegreen_dragon@hotmail.com>
2023-04-19 14:22:58 +0200

    Worked on form.js, adding declarables for regex.js

--- Public/JS/form.js
+++ Public/JS/form.js
@@ -1,5 +1,29 @@
 const db = firebase.firestore();
 
+let parentList = [];
+let age;
+let childIndex = 1;
+
+const grunnleggendeDataForm = document.querySelector('.grunnleggendeData');
+const økonomiskeForholdForm = document.querySelector('.okonomiskeForhold');
+const livssituasjonForm = document.querySelector('.livsSituasjon');
+const NAVForm = document.querySelector('.NAV');
+const lånekassenForm = document.querySelector('.lanekassen');
+const arbeidsForholdForm = document.querySelector('.arbeidsForhold');
+const inntektFortidForm = document.querySelector('.inntektFortid');
+const inntekt2022Form = document.querySelector('.inntekt2022');
+
+const submitBtn = document.querySelector('.submitKnapp');
+const toggle1Btn = document.querySelector('.suggestions1DropBtn');
+const toggle2Btn = document.querySelector('.suggestions2DropBtn');
+let statusField = document.querySelector("#status");
+let statusFieldDead = document.querySelector("#statusDead");
+let statusChangeField = document.querySelector("#endringStatus");
+let statusChangeFieldLabel = document.querySelector("#endringStatusLabel");
+const countryField = grunnleggendeDataForm.land;
+let childArea = Array.from(document.querySelectorAll("div.childArea input"));
+const childFields = grunnleggendeDataForm.barn.parentElement.parentElement;
+
 const fornavn = document.getElementById("fornavn")
 const etternavn = document.getElementById("etternavn")
 const fodselsdato = document.getElementById("fodselsdato")
@@ -9,6 +33,9 @@ const ekteskap = document.getElementById("ekteskap")
 const ektefelle = document.getElementById("ektefelle")
 const forelder1 = document.getElementById("forelder1")
 const forelder2 = document.getElementById("forelder2")
+const barn = document.getElementById("barn")
+const land = document.getElementById("land")
+const botid = document.getElementById("botid")
 
 let fields = [fornavn, etternavn, fodselsdato, livsstatus, endringStatus, ekteskap, ektefelle, forelder1, forelder2]
 console.log(fields);
@@ -39,6 +66,10 @@ function grabIdInfo() {
         ektefelle.value = doc.data().ektefelle
         forelder1.value = doc.data().foreldre[0]
         forelder2.value = doc.data().foreldre[1]
+        barn.value = doc.data().barn
+        land.value = doc.data().land
+        botid.value = doc.data().botid
+
     }).catch((error) => {
         console.log("Error getting cached document:", error);
     });
